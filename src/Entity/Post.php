edd4ca471d60756d3c43d97b8d18fd83d31d90c8
unknown <benmassoudrayen7@gmail.com>
2024-02-11 20:09:11 +0100

    addPost

---
+++ src/Entity/Post.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\PostTypeEnum;
+use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(length: 255,enumType:PostTypeEnum::class)]
+    private ?PostTypeEnum $type = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $album = [];
+
+    #[ORM\Column(length: 255)]
+    private ?string $place = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getAlbum(): array
+    {
+        return $this->album;
+    }
+
+    public function setAlbum(array $album): static
+    {
+        $this->album = $album;
+
+        return $this;
+    }
+
+    public function getPlace(): ?string
+    {
+        return $this->place;
+    }
+
+    public function setPlace(string $place): static
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+}
---
+++ src/Enum/PostTypeEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enum;
+
+enum PostTypeEnum: string {
+    case LOST = 'LOST';
+    case FOUND = 'FOUNED';
+}
+
