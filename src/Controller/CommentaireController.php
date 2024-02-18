9207b26092c47a2b99e99db61018097e83c58387
Mayssabenjoud <mayssa.benjoud@esprit.tn>
2024-02-18 01:19:56 +0100

    commentairepostgroup

---
+++ src/Controller/CommentaireController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Postcommentaire;
+use App\Entity\PostGroup;
+use App\Form\FormCommentaireType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentaireController extends AbstractController
+{
+    #[Route('/commentaire', name: 'app_commentaire')]
+    public function index(): Response
+    {
+        return $this->render('commentaire/index.html.twig', [
+            'controller_name' => 'CommentaireController',
+        ]);
+    }
+    
+
+    
+}
