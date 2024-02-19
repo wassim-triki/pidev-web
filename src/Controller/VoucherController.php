46f6811343dbc8a92853e519af7fc5086a17edef
YASSCODER <mohamedyassine.benaoun@gmail.com>
2024-02-19 17:58:04 +0100

    fix interface to display vouchers

--- src/Controller/VoucherController.php
+++ src/Controller/VoucherController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Voucher;
 class VoucherController extends AbstractController
 {
+    private $managerRegistry;
+
+    public function __construct(ManagerRegistry $managerRegistry)
+    {
+        $this->managerRegistry = $managerRegistry;
+    }
+
     #[Route('/voucher', name: 'app_voucher')]
     public function index(): Response
     {
         return $this->render('voucher/index.html.twig', [
-            'controller_name' => 'VoucherController',
+            'voucher' => $this->managerRegistry->getRepository(Voucher::class)->findAll(),
         ]);
     }
 }
+
