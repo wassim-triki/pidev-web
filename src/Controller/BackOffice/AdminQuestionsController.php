9606ba43c258a9bad6e9d04be83f9a5240345d21
CharFounix <ashrefbouden@gmail.com>
2024-03-06 02:45:15 +0100

    QA stats work correctly :)

--- src/Controller/BackOffice/AdminQuestionsController.php
+++ src/Controller/BackOffice/AdminQuestionsController.php
@@ -16,6 +16,9 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
+
 class AdminQuestionsController extends AbstractController
 {
     #[Route('/questions', name: 'app_admin_questions')]
@@ -26,79 +29,90 @@ class AdminQuestionsController extends AbstractController
         ]);
     }
     #[Route('/showquestions', name: 'admin_questions_show')]
-public function show(QuestionRepository $questionRepository,PaginatorInterface $paginator,Request $req): Response
-{
-    $pagination = $paginator->paginate(
-        $questionRepository->paginationquery(),
-        $req->query->get('page',1),
-        3
+    public function show(QuestionRepository $questionRepository, PaginatorInterface $paginator, Request $req): Response
+    {
+        $pagination = $paginator->paginate(
+            $questionRepository->paginationquery(),
+            $req->query->get('page', 1),
+            3
 
-    );
-    $totalQuestions = $questionRepository->getTotalQuestionsCount();
-   $answeredQuestions = $questionRepository->getAnsweredQuestionsCount();
-   $todayQuestions = $questionRepository->getTodayQuestionsCount();
+        );
+        $totalQuestions = $questionRepository->getTotalQuestionsCount();
+        $answeredQuestions = $questionRepository->getAnsweredQuestionsCount();
+        $todayQuestions = $questionRepository->getTodayQuestionsCount();
 
 
-    return $this->render('back_office/admin_questions/show.html.twig', [
-        'questions' => $pagination,
-        'total_questions' => $totalQuestions,
-        'answered_questions' => $answeredQuestions,
-        'today_questions' => $todayQuestions,
-    ]);
-}
-#[Route('/question/{id}/answer', name: 'admin_answer_new')]
-public function newAnswer(Request $request, ManagerRegistry $managerRegistry, Question $question,MailerInterface $mailer): Response
-{
-    $answer = $question->getAnswer();
-
-    if (!$answer) {
-        $answer = new Answer();
-        $answer->setQuestionId($question);
+        return $this->render('back_office/admin_questions/show.html.twig', [
+            'questions' => $pagination,
+            'total_questions' => $totalQuestions,
+            'answered_questions' => $answeredQuestions,
+            'today_questions' => $todayQuestions,
+        ]);
     }
+    #[Route('/question/{id}/answer', name: 'admin_answer_new')]
+    public function newAnswer(Request $request, ManagerRegistry $managerRegistry, Question $question, MailerInterface $mailer): Response
+    {
+        $answer = $question->getAnswer();
 
-    $form = $this->createForm(AnswerType::class, $answer);
-    $form->handleRequest($request);
+        if (!$answer) {
+            $answer = new Answer();
+            $answer->setQuestionId($question);
+        }
 
-    if ($form->isSubmitted() && $form->isValid()) {
+        $form = $this->createForm(AnswerType::class, $answer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $managerRegistry->getManager();
+            $answer->setCreatedAt(new \DateTimeImmutable());
+            $entityManager->persist($answer);
+            $entityManager->flush();
+            $email = (new TemplatedEmail())
+                ->from('admin@Al9ani.tn')
+                ->to($question->getUserId()->getEmail())
+                ->subject('Votre question a été répondue par un administrateur !')
+                ->htmlTemplate('back_office/admin_questions/email.html.twig')
+
+                ->context([
+                    'question' => $question,
+                    'answer' => $answer,
+                    'name' => $question->getUserId()
+
+
+                ])
+            ;
+
+            $mailer->send($email);
+
+
+            return $this->redirectToRoute('admin_questions_show', ['id' => $question->getId()]);
+        }
+
+        return $this->render('back_office/admin_questions/new.html.twig', [
+            'answer' => $answer,
+            'form' => $form->createView(),
+        ]);
+    }
+    #[Route('/admin/answer/{id}/delete', name: 'admin_answer_delete')]
+
+    public function deleteAnswer(Answer $answer, ManagerRegistry $managerRegistry): Response
+    {
         $entityManager = $managerRegistry->getManager();
-        $answer->setCreatedAt(new \DateTimeImmutable());
-        $entityManager->persist($answer);
+        $entityManager->remove($answer);
         $entityManager->flush();
-        $email = (new TemplatedEmail())
-            ->from('admin@Al9ani.tn')
-            ->to($question->getUserId()->getEmail())
-            ->subject('Votre question a été répondue par un administrateur !')
-            ->htmlTemplate('back_office/admin_questions/email.html.twig')
-        
-            ->context([
-                'question' =>$question,
-                'answer'=>$answer,
-                'name'=>$question->getUserId()
 
-                
-            ])
-        ;
-
-        $mailer->send($email);
-
-
-        return $this->redirectToRoute('admin_questions_show', ['id' => $question->getId()]);
+        return $this->redirectToRoute('admin_questions_show', ['id' => $answer->getQuestionId()->getId()]);
     }
-
-    return $this->render('back_office/admin_questions/new.html.twig', [
-        'answer' => $answer,
-        'form' => $form->createView(),
-    ]);
-}
-#[Route('/admin/answer/{id}/delete', name: 'admin_answer_delete')]
-
-public function deleteAnswer(Answer $answer, ManagerRegistry $managerRegistry): Response
-{
-    $entityManager = $managerRegistry->getManager();
-    $entityManager->remove($answer);
-    $entityManager->flush();
-
-    return $this->redirectToRoute('admin_questions_show', ['id' => $answer->getQuestionId()->getId()]);
+    #[Route('admin/archivequestion/{id}', name: 'archive_question_admin')]
+    public function deletequestion($id, QuestionRepository $qr, ManagerRegistry $managerRegistry): Response
+    {
+        $em = $managerRegistry->getManager();
+        $dataid = $qr->find($id);
+        $em->remove($dataid);
+        $em->flush();
+        return $this->redirectToRoute('admin_questions_show');
+    }
+ 
+    
 }
 
-}
